3f9ad9b4dfb5e1dfb13a2d26157ab1ac5d2f76bc
JakeTurner616 <jake@serverboi.org>
2022-10-03 02:22:44 -0600

    Update reboot-to-bios.bat

--- reboot-to-bios.bat
+++ reboot-to-bios.bat
@@ -1,6 +1,7 @@
 @echo off
-ECHO ===================================
-ECHO SYSTEM WILL REBOOT TO BIOS IN 5 SEC
-ECHO ===================================
+ECHO ================================================
+ECHO SYSTEM WILL REBOOT TO BIOS IF SUPPORTED IN 5 SEC
+ECHO ================================================
 timeout /t 5
-shutdown /r /fw /f /t 0
+shutdown /r /fw /f 
+PAUSE
